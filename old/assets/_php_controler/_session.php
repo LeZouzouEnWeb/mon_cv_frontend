8948cdc772de2ffb4e16f751d06a4c8f81061d97
Eric CORBISIER <contact@corbisier.fr>
2025-12-17 18:20:10 +0100

    Supression du précédent site

--- old/assets/_php_controler/_session.php
+++
@@ -1,7 +0,0 @@
-<?
-//********REJET ACCES DIRECT********
-defined('CORBTECH_SECUR_ROOT_PATH') or die("°_°'");
-//**********************************
-
-require_once DIR_ASSETS . "/_php_controler/_variables.php";
-require_once DIR_ASSETS . "/_php_functions/_functions.php";
